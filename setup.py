452c3c4258c8dce5bd6f9e6799fe24253a800651
Bo Lin <bo@dreamsphere.org>
2014-06-12 11:56:10 -0400

    Change project name (back) to "DistAlgo".
    * setup.py: Change project name to "DistAlgo".

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 import dpy
 
-setup(name = "DistPy",
+setup(name = "DistAlgo",
       version = dpy.__version__,
       author= "bolin",
       author_email = "bolin@cs.stonybrook.edu",
