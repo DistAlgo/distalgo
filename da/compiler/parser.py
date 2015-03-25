DistAlgo/distalgo

commit 9e38876537c71046ef72a257299defd97ad303c1

Add type annotation support for incgen.
* da/compiler/dast.py:
  - (Arguments): add annotation parameter for arguments, pass along to
    NamedVar instance.
  - (NamedVar): default typectx changed from 'object' to 'None'.
  - (NamedVar.type): new property, returns type expression for this name.

* da/compiler/incgen.py:
  - (STUB_ASSIGN, STUB_ASSIGN_JB, STUB_ASSIGN_JB_SET): new constants,
    formatting for different assignment stubs.
  - (gen_assign_stub): add stub for sets when jbstyling; prefix stub argument
    name with '_' to distinguish between argument and variable.

* da/compiler/parser.py:
  - (Parser.signature): parse annotation for arguments.
  - (Parser.visit_ClassDef, Parser.visit_FunctionDef): more debugging output.

* examples/*, benchmarks/*:
  - Add annotations for 'setup' methods.

da/compiler/parser.py
@@ -707,23 +707,43 @@ class Parser(NodeVisitor):
         padding = len(node.args) - len(node.defaults)
         container = self.current_parent.args
         for arg in node.args[:padding]:
-            container.add_arg(arg.arg)
+            if arg.annotation is not None:
+                annotation = self.visit(arg.annotation)
+                container.add_arg(arg.arg, annotation)
+            else:
+                container.add_arg(arg.arg)
         for arg, val in zip(node.args[padding:], node.defaults):
-            container.add_defaultarg(arg.arg, self.visit(val))
+            if arg.annotation is not None:
+                annotation = self.visit(arg.annotation)
+                container.add_defaultarg(arg.arg, self.visit(val), annotation)
+            else:
+                container.add_defaultarg(arg.arg, self.visit(val))
         if node.vararg is not None:
             # Python 3.4 compatibility:
             if type(node.vararg) is str:
                 container.add_vararg(node.vararg)
             else:
-                container.add_vararg(node.vararg.arg)
+                if arg.annotation is not None:
+                    annotation = self.visit(arg.annotation)
+                    container.add_vararg(node.vararg.arg, annotation)
+                else:
+                    container.add_vararg(node.vararg.arg)
         if node.kwarg is not None:
             # Python 3.4 compatibility:
             if type(node.kwarg) is str:
                 container.add_kwarg(node.kwarg)
             else:
-                container.add_vararg(node.kwarg.arg)
+                if node.kwarg.annotation is not None:
+                    annotation = self.visit(node.kwarg.annotation)
+                    container.add_vararg(node.kwarg.arg, annotation)
+                else:
+                    container.add_vararg(node.kwarg.arg)
         for kwarg, val in zip(node.kwonlyargs, node.kw_defaults):
-            container.add_kwonlyarg(kwarg.arg, self.visit(val))
+            if kwarg.annotation is not None:
+                annotation = self.visit(kwarg.annotation)
+                container.add_kwonlyarg(kwarg.arg, self.visit(val), annotation)
+            else:
+                container.add_kwonlyarg(kwarg.arg, self.visit(val))
 
 
     # Top-level blocks:
@@ -761,6 +781,10 @@ class Parser(NodeVisitor):
             # setup() has to be parsed first:
             self.proc_body([node.body[bodyidx]] +
                            node.body[:bodyidx] + node.body[(bodyidx+1):])
+            dbgstr = ["Process ", proc.name, " has names: "]
+            for n in proc._names.values():
+                dbgstr.append("%s: %s; " % (n, str(n.type)))
+            self.debug("".join(dbgstr))
             self.pop_state()
             if proc.entry_point is None:
                 self.warn("Process %s missing '%s()' method." %
@@ -780,6 +804,10 @@ class Parser(NodeVisitor):
             self.push_state(clsobj)
             self.current_block = clsobj.body
             self.body(node.body)
+            dbgstr = ["Class ", proc.name, " has names: "]
+            for n in proc._names.values():
+                dbgstr.append("%s: %s; " % (n, str(n.type)))
+            self.debug("".join(dbgstr))
             self.pop_state()
 
     def visit_FunctionDef(self, node):
@@ -807,6 +835,10 @@ class Parser(NodeVisitor):
             if not self.is_in_setup():
                 self.signature(node.args)
             self.body(node.body)
+            dbgstr = [s.name, " has names: "]
+            for n in s._names.values():
+                dbgstr.append(("%s: %s; " % (n, str(n.type))))
+            self.debug("".join(dbgstr))
             self.pop_state()
 
         else:

benchmarks/2pcommit/orig.da
@@ -3,7 +3,7 @@ from random import randint
 controller = import_da('controller')
 
 class Coordinator(process, controller.Controllee):
-    def setup(ctl, cohorts):
+    def setup(ctl, cohorts:set):
         super().setup(ctl)
 
     def to_commit():

benchmarks/2pcommit/spec.da
@@ -3,7 +3,7 @@ from random import randint
 controller = import_da('controller')
 
 class Coordinator(process, controller.Controllee):
-    def setup(ctl, tid, cohorts):
+    def setup(ctl, tid, cohorts:set):
         super().setup(ctl)
 
     def to_commit():

benchmarks/clpaxos/spec.da
@@ -2,7 +2,7 @@ import sys
 controller = import_da('controller')
 
 class Proposer(process, controller.Controllee):
-    def setup(ctl, acceptors, quorumsize, f, nrounds, timeout):
+    def setup(ctl, acceptors:set, quorumsize, f, nrounds, timeout):
         super().setup(ctl)
         self.propNum = (0, self.id)          # Current proposal(ballot) number
         self.propVal = self.id          # Own value to propose
@@ -37,7 +37,7 @@ class Proposer(process, controller.Controllee):
         send(('Done',), to=acceptors)
 
 class Acceptor(process, controller.Controllee):
-    def setup(ctl, acceptors, proposers, quorumsize, f):
+    def setup(ctl, acceptors:set, proposers:set, quorumsize, f):
         super().setup(ctl)
         self.peers = acceptors | proposers
 

benchmarks/dscrash/orig.da
@@ -2,7 +2,7 @@ import sys
 controller = import_da('controller')
 
 class P(process, controller.Controllee):
-    def setup(ctl, ps, v, maxfail):
+    def setup(ctl, ps:set, v, maxfail):
         super().setup(ctl)
         self.x = -1
         self.V = {v: False}

benchmarks/dscrash/spec.da
@@ -2,7 +2,7 @@ import sys
 controller = import_da('controller')
 
 class P(process, controller.Controllee):
-   def setup(ctl, ps, v, maxfail):
+   def setup(ctl, ps:set, v, maxfail):
        super().setup(ctl)
        self.V = {v}          # set of values to agree on, intially own value v
 

benchmarks/lamutex/orig.da
@@ -2,7 +2,7 @@ import sys
 controller = import_da('controller')
 
 class P(process, controller.Controllee):
-    def setup(ctl, s, nrequests):  # s is set of all other processes
+    def setup(ctl, s:set, nrequests):  # s is set of all other processes
         super().setup(ctl)
         self.q = set()
 

benchmarks/lamutex/spec.da
@@ -2,7 +2,7 @@ import sys
 controller = import_da('controller')
 
 class P(process, controller.Controllee):
-    def setup(ctl, s, nrequests):  # s is set of all other processes
+    def setup(ctl, s:set, nrequests):  # s is set of all other processes
         super().setup(ctl)
 
     def mutex(task):

commit 6154294baf41687371819c57432a1add04f99d13

Parser: fix wrong name in visit_classdef.
This fixes a bug introduced in 9e38876537c71046ef72a257299defd97ad303c1.

* da/compiler/parser.el(Parser.visit_ClassDef): change 'proc' to
  'clsobj' in the debug string in the branch for normal classes.

da/compiler/parser.py
@@ -799,13 +799,12 @@ class Parser(NodeVisitor):
                 self.current_block.append(clsobj)
                 n = self.current_scope.add_name(node.name)
                 n.add_assignment(clsobj)
-            self.current_context = Read()
             clsobj.decorators, _, _ = self.parse_decorators(node)
             self.push_state(clsobj)
             self.current_block = clsobj.body
             self.body(node.body)
-            dbgstr = ["Class ", proc.name, " has names: "]
-            for n in proc._names.values():
+            dbgstr = ["Class ", clsobj.name, " has names: "]
+            for n in clsobj._names.values():
                 dbgstr.append("%s: %s; " % (n, str(n.type)))
             self.debug("".join(dbgstr))
             self.pop_state()