DistAlgo/distalgo

commit 04743b6fe10fc0baefca0ccc20d1660b329162fd

Updated jbstyle for invinc runtimelib change.

da/compiler/incgen.py
@@ -314,7 +314,7 @@ def {0}():
     return globals()['{1}']
 """ if not Options.jb_style else """
 def {0}():
-    globals()['{1}'] = runtimelib.Set()
+    globals()['{1}'] = invinc.runtime.Set()
     return globals()['{1}']
 """
 
@@ -335,7 +335,7 @@ def gen_inc_module(daast, cmdline_args, filename=""):
     module = parse(PREAMBLE)
     if Options.jb_style:
         # Additional import for jb_style
-        module.body.insert(0, parse("import runtimelib").body[0])
+        module.body.insert(0, parse("import invinc.runtime").body[0])
     quex = QueryExtractor()
     quex.visit(daast)
 

commit 4bffcce290dd2eafc3215e843200cfaecf2221f2

Incgen: fix name inconsistency when not using jbstyle.
This fixes a bug introduced by 04743b6fe10fc0baefca0ccc20d1660b329162fd.

* da/compiler/incgen.py(STUB_ASSIGN): add underscore prefix to argument
  name.

da/compiler/incgen.py
@@ -271,13 +271,13 @@ def is_a(nameobj, typexpr):
         return False
 
 STUB_ASSIGN = """
-def {1}({0}):
-    if type({0}) is set:
-        res = set()
+def {1}(_{0}):
+    global {0}
+    if type(_{0}) is set:
+        {0} = set()
         for elt in {0}:
-            res.add(elt)
-        {0} = res
-    {0} = {0}
+            {0}.add(elt)
+    {0} = _{0}
     return {0}
 """
 STUB_ASSIGN_JB = """