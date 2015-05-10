3ffbbe0626ff2bdca958db006ccd54bf93a16587
Bo Lin <bo@dreamsphere.org>
2015-05-10 11:41:50 -0400

    * setup.py: Add classifiers.

--- setup.py
+++ setup.py
@@ -6,4 +6,11 @@ setup(name = "DistAlgo",
       version = da.__version__,
       author= "bolin",
       author_email = "bolin@cs.stonybrook.edu",
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development :: Compilers',
+      ],
       packages = ['da', 'da.compiler', 'da.tools'])
