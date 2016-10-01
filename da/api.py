DistAlgo/distalgo

commit 1e79c9cd3e6bbd59a967a9ae2b9a0e48da76af34

logging: put pid into "extra" instead of as part of logger name.
Logger names are used in creating a parent-child relationship between
loggers that determine inheritance of logging configurations. Putting
pid in the logger name may break logger inheritance.

This patch changes our log format so that process ids can be imparted as
"extra" contextual information, and removes process ids from logger
names. It also fixes the `@deprecated` decorator.

* da/common.py:
  -(CurrentProcess): initialize to sane value.
  -(DALogger): new class, custom logger class that sets the `extra` dict
   to `CurrentProcess` in log records if none is specified. This class
   is used as the default logger class throughout DistAlgo runtime.
  -(set_current_process): updates `CurrentProcess`.
  -(setup_root_logger): update log format to use `pid`.
  -(deprecated): set `warnings` packages to show `DeprecationWarning`s;
   set `stacklevel` to 2 since we're in a wrapper function.
  -(api): replace deprecated `log.warn` with `log.warning`.

* da/sim.py:
  -(DistProcess.__init__): use qualified class name as logger name.
  -(DistProcess.__process_event): replace deprecated `log.warn` with
   `log.warning`.
  -(Comm.__init__): use qualified class name as logger name.
  -(Comm.run): replace deprecated `log.warn` with `log.warning`.
  -(OSProcessImpl.__init__): use qualified class name as logger name.
  -(OSProcessImpl.run): update for new signature of
   `set_current_process`.

da/common.py
@@ -27,24 +27,36 @@ import sys
 import copy
 import os.path
 import logging
-import collections.abc
 import importlib
+import warnings
+import multiprocessing
+import collections.abc
 
 from inspect import signature, Parameter
 from functools import wraps
 
 GlobalOptions = None
-CurrentProcess = None
+CurrentProcess = {'pid' : multiprocessing.current_process().pid}
 IncOQBaseType = None           # incoq.runtime.Type, if using incoq
 
+class DALogger(logging.getLoggerClass()):
+    """Custom logger that attaches current process info.
+
+    """
+    def _log(self, level, msg, args, exc_info=None, extra=None):
+        if extra is None:
+            extra = CurrentProcess
+        return super()._log(level, msg, args, exc_info, extra)
+
+logging.setLoggerClass(DALogger)
 log = logging.getLogger(__name__)
 
 api_registry = dict()
 builtin_registry = dict()
 
-def set_current_process(procobj):
+def set_current_process():
     global CurrentProcess
-    CurrentProcess = procobj
+    CurrentProcess['pid'] = multiprocessing.current_process().pid
 
 def set_global_options(params):
     global GlobalOptions
@@ -73,7 +85,7 @@ def setup_root_logger():
         formatter = logging.Formatter(
             '[%(asctime)s]%(name)s:%(levelname)s: %(message)s')
         consoleformatter = logging.Formatter(
-            '%(name)s:%(levelname)s: %(message)s')
+            '%(name)s<%(pid)s>:%(levelname)s: %(message)s')
         rootlog._formatter = formatter
 
         consolelvl = logging._nameToLevel[GlobalOptions.logconsolelevel.upper()]
@@ -118,16 +130,17 @@ def load_modules():
     else:
         main.IncModule = inc
 
+warnings.simplefilter("default", DeprecationWarning)
 def deprecated(func):
     """Declare 'func' as deprecated.
 
     This is a decorator which can be used to mark functions as deprecated. It
     will result in a warning being emmitted when the function is used.
-    """
 
+    """
     def newFunc(*args, **kwargs):
         warnings.warn("Call to deprecated function %s." % func.__name__,
-                      category=DeprecationWarning)
+                      category=DeprecationWarning, stacklevel=2)
         return func(*args, **kwargs)
     newFunc.__name__ = func.__name__
     newFunc.__doc__ = func.__doc__
@@ -168,7 +181,7 @@ def api(func):
         result = func(*args, **kwargs)
         if (sig.return_annotation is not Parameter.empty and
                 not isinstance(result, sig.return_annotation)):
-            log.warn(
+            log.warning(
                 ("Possible bug: API function '%s' return value type mismatch: "
                  "declared %s, returned %s.") %
                 (funame, sig.return_annotation, result.__class__))
@@ -345,5 +358,10 @@ if __name__ == "__main__":
     testapi(1, {})
     print(api_registry)
 
+    @deprecated
+    def testdepre():
+        print("deprecated function")
+
     t = ('a', 1)
     print("Freeze " + str(t) + "->" + str(freeze(t)))
+    testdepre()

da/sim.py
@@ -104,7 +104,7 @@ class DistProcess():
         else:
             self._logical_clock = None
         self._dp_name = procimpl.procname
-        self._log = logging.getLogger(str(self))
+        self._log = logging.getLogger(self.__class__.__qualname__)
 
     def setup(self):
         pass
@@ -343,7 +343,7 @@ class DistProcess():
             if not isinstance(event.timestamp, int):
                 # Most likely some peer did not turn on lamport clock, issue
                 # a warning and skip this message:
-                self._log.warn(
+                self._log.warning(
                     "Invalid logical clock value: {0}; message dropped. "
                     "".format(event.timestamp))
                 return False
@@ -401,7 +401,7 @@ class Comm(threading.Thread):
         self.condition = threading.Condition()
         self.num_waiting = 0
         self.q = collections.deque()
-        self.log = logging.getLogger(str(self))
+        self.log = logging.getLogger(self.__class__.__qualname__)
 
     def run(self):
         try:
@@ -414,7 +414,7 @@ class Comm(threading.Thread):
             try:
                 (src, clock, data) = msg
             except ValueError as e:
-                self.log.warn("Invalid message dropped: {0}".format(str(msg)))
+                self.log.warning("Invalid message dropped: {0}".format(str(msg)))
                 continue
 
             e = pattern.ReceivedEvent(
@@ -447,7 +447,7 @@ class OSProcessImpl(multiprocessing.Process):
         self.daemon = props['daemon'] if 'daemon' in props else False
         self.procname = name
         self.endpoint = None
-        self._log = logging.getLogger(str(self))
+        self._log = logging.getLogger(self.__class__.__qualname__)
         self._child_procs = []
 
     def _wait_for_go(self):
@@ -494,6 +494,7 @@ class OSProcessImpl(multiprocessing.Process):
             if multiprocessing.get_start_method() == 'spawn':
                 common.set_global_options(self._cmdline)
                 common.sysinit()
+            common.set_current_process()
             signal.signal(signal.SIGTERM, self._sighandler)
             signal.signal(signal.SIGUSR1, self._debug_handler)
             if self.procname is None:
@@ -501,7 +502,6 @@ class OSProcessImpl(multiprocessing.Process):
             self.endpoint = self._dacls.get_channel_type()(self._dacls)
             self._log = logging.getLogger(str(self))
             self._start_comm_thread()
-            common.set_current_process(self.endpoint)
             pattern.initialize(self.endpoint)
             self._daobj = self._dacls(self, self._properties)
             self._wait_for_go()

commit 1f5392c3566e0257a0458f666a43b1c3fcff94d2

logging: various fixes.
Fix a bug introduced by 1e79c9cd3e6bbd59a967a9ae2b9a0e48da76af34 where a
non-picklable logger object is created as a field of `OSProcessImpl` by
the `__init__` method, breaking spawning semantics.

Fix passing pre-formatted strings to loggers instead of delegating log
formatting to the logger. Pre-formatting log messages creates
unnecessary overhead.

Improves console log message format.

* da/api.py:
  -(entrypoint, new): don't pre-format log messages.

* da/common.py:
  -(setup_root_logger): use `relativeCreated` time to show relative
   ordering of log events without using up too much space.

* da/sim.py:
  -(DistProcess)
   -(.__init__): use `OSProcessImpl.name`.
   -(.get_channel_type, ._label, .incr_logical_clock, .__process_event,
     .__trigger_event): don't pre-format log messages.
  -(Comm.mesgloop):  don't pre-format log messages.
  -(OSProcessImpl.__init__): don't initialize `self._log` here, do it in
   the child process' address space; use `.name` property inherited from
   `Process` instead of custom `.procname` property.
  -(OSProcessImpl._wait_for_go): don't pre-format log messages.
  -(OSProcessImpl.run): create logger instance for the process.

da/api.py
@@ -156,12 +156,12 @@ def entrypoint():
     common.sysinit()
 
     # Start main program
-    log.info("Starting program {0}...".format(target))
+    log.info("Starting program %s...", target)
     niters = GlobalOptions.iterations
     sys.argv = [target] + GlobalOptions.args
     try:
         for i in range(0, niters):
-            log.info("Running iteration %d ..." % (i+1))
+            log.info("Running iteration %d ...", (i+1))
 
             node = new(module._NodeMain, args=())
             start(node)
@@ -182,10 +182,10 @@ def entrypoint():
 @api
 def new(pcls, args=None, num=None, **props):
     if not issubclass(pcls, sim.DistProcess):
-        log.error("Can not create non-DistProcess.")
+        log.error("Can not create non-DistProcess classes.")
         return set()
 
-    log.debug("Creating instances of %s.." % str(pcls))
+    log.debug("Creating instances of %s..", pcls.__name__)
     pipes = []
     iterator = []
     if num is None:
@@ -195,7 +195,7 @@ def new(pcls, args=None, num=None, **props):
     elif isinstance(num, collections.abc.Iterable):
         iterator = num
     else:
-        log.error("Unrecognised parameter: %s", str(num))
+        log.error("Invalid value for `num`: %r", num)
         return set()
 
     procs = set()

da/common.py
@@ -85,9 +85,9 @@ def setup_root_logger():
     if not GlobalOptions.nolog:
         rootlog.setLevel(logging.DEBUG)
         formatter = logging.Formatter(
-            '[%(asctime)s]%(name)s:%(levelname)s: %(message)s')
+            '[%(asctime)s] %(name)s:%(levelname)s: %(message)s')
         consoleformatter = logging.Formatter(
-            '%(name)s<%(pid)s>:%(levelname)s: %(message)s')
+            '[%(relativeCreated)d] %(processName)s<%(pid)s>:%(levelname)s: %(message)s')
         rootlog._formatter = formatter
 
         consolelvl = logging._nameToLevel[GlobalOptions.logconsolelevel.upper()]

da/sim.py
@@ -103,7 +103,7 @@ class DistProcess():
             self._logical_clock = 0
         else:
             self._logical_clock = None
-        self._dp_name = procimpl.procname
+        self._dp_name = procimpl.name
         self._log = logging.getLogger(self.__class__.__qualname__)
 
     def setup(self):
@@ -125,7 +125,7 @@ class DistProcess():
             elif prop == "reliable":
                 ept = endpoint.TcpEndPoint
             elif prop not in {"unfifo", "unreliable"}:
-                log.error("Unknown channel property %s", str(prop))
+                log.error("Unknown channel property %r", prop)
         return ept
 
     _config_object = dict()
@@ -187,7 +187,8 @@ class DistProcess():
     def _send(self, message, to):
         self.incr_logical_clock()
         if (self.__fails('send')):
-            self.output("Simulated send fail: %s" % str(data), logging.WARNING)
+            self._log.warning(
+                "Dropped outgoing message due to lottery: %r", message)
             return
 
         if (hasattr(to, '__iter__')):
@@ -224,10 +225,10 @@ class DistProcess():
 
         """
         if self.__fails('hang'):
-            self.output("Hanged(@label %s)" % name, logging.WARNING)
+            self._log.warning("Hanged(@label %s)", name)
             self._lock.acquire()
         if self.__fails('crash'):
-            self.output("Crashed(@label %s)" % name, logging.WARNING)
+            self._log.warning("Crashed(@label %s)", name)
             self.exit(10)
 
         self.__do_label(name, block, timeout)
@@ -311,12 +312,13 @@ class DistProcess():
             return False
 
         if event is None:
-            self._log.debug("__process_event: event was stolen by another thread.")
+            if block:
+                self._log.debug(
+                    "__process_event: event was stolen by another thread.")
             return False
 
         if self.__fails('receive'):
-            self.output("Simulated receive fail: %s" % str(mesg),
-                        logging.WARNING)
+            self._log.warning("Dropped incoming message due to lottery: %s", event)
             return False
 
         if isinstance(self._logical_clock, int):
@@ -324,8 +326,8 @@ class DistProcess():
                 # Most likely some peer did not turn on lamport clock, issue
                 # a warning and skip this message:
                 self._log.warning(
-                    "Invalid logical clock value: {0}; message dropped. "
-                    "".format(event.timestamp))
+                    "Invalid logical clock value: %r; message dropped. ",
+                    event.timestamp)
                 return False
             self._logical_clock = max(self._logical_clock, event.timestamp) + 1
         self.__trigger_event(event)
@@ -335,7 +337,7 @@ class DistProcess():
         """Immediately triggers 'event', skipping the event queue.
 
         """
-        self._log.debug("triggering event %s" % event)
+        self._log.debug("triggering event %s", event)
         for p in self._events:
             bindings = dict()
             if (p.match(event, bindings=bindings,
@@ -393,7 +395,7 @@ class Comm(threading.Thread):
             try:
                 (src, clock, data) = msg
             except ValueError as e:
-                self.log.warning("Invalid message dropped: {0}".format(str(msg)))
+                self.log.warning("Dropped invalid message: %r", msg)
                 continue
 
             e = pattern.ReceivedEvent(envelope=(clock, None, src),
@@ -408,7 +410,10 @@ class OSProcessImpl(multiprocessing.Process):
     def __init__(self, dacls, initpipe, name=None, props=None):
         assert issubclass(dacls, DistProcess)
 
-        multiprocessing.Process.__init__(self)
+        super().__init__()
+        # Logger can not be serialized so it has to be instantiated in the child
+        # proc's address space:
+        self._log = None
         self._dacls = dacls
         self._daobj = None
         self._running = False
@@ -420,9 +425,11 @@ class OSProcessImpl(multiprocessing.Process):
         else:
             self._properties = dict()
         self.daemon = props['daemon'] if 'daemon' in props else False
-        self.procname = name
+        if name is not None:
+            self.name = name
+        else:
+            self.name = dacls.__qualname__
         self.endpoint = None
-        self._log = logging.getLogger(self.__class__.__qualname__)
         self._child_procs = []
 
     def _wait_for_go(self):
@@ -434,18 +441,19 @@ class OSProcessImpl(multiprocessing.Process):
             if act == Command.Start:
                 self._running = True
                 del self._initpipe
-                self._log.debug("'start' command received, commencing...")
+                self._log.debug("`start` command received, commencing...")
                 return
             else:
                 inst, args = act
                 if inst == Command.Setup:
                     if self._setup_called:
-                        self._log.warn(
-                            "setup() already called for this process!")
+                        self._log.warning(
+                            "`setup` already called for this process!")
                     else:
-                        self._log.debug("Running setup..")
+                        self._log.debug("Running `setup`...")
                         self._daobj.setup(*args)
                         self._setup_called = True
+                        self._log.debug("`setup` complete.")
                 else:
                     m = getattr(self, "set_" + inst)
                     m(*args)
@@ -464,6 +472,7 @@ class OSProcessImpl(multiprocessing.Process):
         self._debugger.set_trace(frame)
 
     def run(self):
+        self._log = logging.getLogger(self.__class__.__qualname__)
         try:
             self._cmdline.this_module_name = self.__class__.__module__
             if multiprocessing.get_start_method() == 'spawn':
@@ -472,10 +481,7 @@ class OSProcessImpl(multiprocessing.Process):
             common.set_current_process()
             signal.signal(signal.SIGTERM, self._sighandler)
             signal.signal(signal.SIGUSR1, self._debug_handler)
-            if self.procname is None:
-                self.procname = str(self.pid)
             self.endpoint = self._dacls.get_channel_type()(self._dacls)
-            self._log = logging.getLogger(str(self))
             self._start_comm_thread()
             pattern.initialize(self.endpoint)
             self._daobj = self._dacls(self, self._properties)