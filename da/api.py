DistAlgo/distalgo

commit d7429a64989fcf26baaaaa550846327ff70f55bb

Simplify configuration logic.
Consolidate runtime configuration value retrieval into
`DistProcess.get_config()'.

* da/api.py:
  -(use_channel): renamed to `init_channel'.
  -(init_channel): creates the channel instance for the root process
   according to configured channel type.
  -(config): removed.
  -(entrypoint): call `init_channel' to initialize the root process
    channel, since `main' no longer calls `use_channel'.

* da/compiler/pygen.py:
  -(CONFIG_API_NAME): removed
  -(CONFIG_OBJECT_NAME): new constant.
  -(PythonGenerator.visit_Program): always generate '_config_object'
   initialization statement at the module level.
  -(PythonGenerator.generate_config): generate assignment to
   '_config_object' instead of call to `config()'.

* da/sim.py:
  -(_config_object): new module level configuration object.
  -(DistProcess.get_config): new instance method, retrieves
   configuration value for given key according to configuration
   priority.
  -(DistProcess.run): use `DistProcess.get_config' to get 'clock'
   configuration.

da/api.py
@@ -131,13 +131,18 @@ def import_da(name, from_dir=None, compiler_args=[]):
 
     return importlib.import_module(name)
 
-def use_channel(channel_properties):
+def init_channel(module):
     global EndPointType
 
     ept = ep.UdpEndPoint
-    if isinstance(channel_properties, str):
-        channel_properties = [channel_properties]
-    for prop in channel_properties:
+    props = []
+    if 'channel' in common.global_options():
+        props = common.global_options()['channel']
+    elif 'channel' in module._config_object:
+        props = module._config_object['channel']
+    if isinstance(props, str):
+        props = [props]
+    for prop in props:
         if prop == "fifo":
             ept = ep.TcpEndPoint
         elif prop == "reliable":
@@ -152,18 +157,6 @@ def use_channel(channel_properties):
                 "Can not change channel type after creating child processes.")
         return
     EndPointType = ept
-
-@api
-def config(**properties):
-    for prop in properties:
-        if prop == 'channel':
-            use_channel(properties['channel'])
-        elif prop == 'clock':
-            setattr(common.global_options(), 'clock', properties[prop])
-        elif prop == 'handling':
-            setattr(common.global_options(), 'handling', properties[prop])
-        else:
-            log.warn("Unknown configuration type '%s'." % str(prop))
 
 def entrypoint():
     GlobalOptions = common.global_options()
@@ -191,6 +184,9 @@ def entrypoint():
     if GlobalOptions.inc_module_name is None:
         GlobalOptions.inc_module_name = module.__name__ + "_inc"
     common.sysinit()
+
+    # Set the default channel type:
+    init_channel(module)
 
     # Start the background statistics thread:
     RootLock.acquire()

da/sim.py
@@ -36,9 +36,11 @@ import traceback
 import collections
 import multiprocessing
 
-from da import pattern, common, endpoint
+from da import api, pattern, common, endpoint
 
 builtin = common.builtin
+
+_config_object = dict()
 
 class DistProcess(multiprocessing.Process):
     """Abstract base class for DistAlgo processes.
@@ -177,17 +179,17 @@ class DistProcess(multiprocessing.Process):
 
     _config_object = dict()
 
-    @classmethod
-    def _configure(cls, **props):
-        cls._config_object.update(props)
-
-    def _get_channel_type(self):
-        result = endpoint.UdpEndPoint
-        if 'channel' in self._get_config():
-            config = self._get_config()['channel']
-            if 'reliable' in config or 'fifo' in config:
-                result = endpoint.TcpEndPoint
-        return result
+    def get_config(self, key, default=None):
+        """Returns the configuration value for specified 'key'.
+        """
+        if key in common.global_options().config:
+            return common.global_options().config[key]
+        elif key in self._config_object:
+            return self._config_object[key]
+        elif key in sys.modules[self.__module__]._config_object:
+            return sys.modules[self.__module__]._config_object[key]
+        else:
+            return default
 
     def run(self):
         try:
@@ -200,8 +202,7 @@ class DistProcess(multiprocessing.Process):
             self.id = self._channel(self._dp_name, self.__class__)
             common.set_current_process(self.id)
             pattern.initialize(self.id)
-            if hasattr(self._cmdline, 'clock') and \
-               self._cmdline.clock == 'Lamport':
+            if self.get_config('clock', 'none').casefold() == 'lamport':
                 self._logical_clock = 0
             self._log = logging.getLogger(str(self))
             self._start_comm_thread()

da/compiler/pygen.py
@@ -73,7 +73,7 @@ if sys.version_info > (3, 5):
 
 PATTERN_EXPR_NAME = "_PatternExpr_%d"
 QUATIFIED_EXPR_NAME = "_QuantifiedExpr_%d"
-CONFIG_API_NAME = "config"
+CONFIG_OBJECT_NAME = "_config_object"
 
 ########## Convenience methods for creating AST nodes: ##########
 
@@ -351,17 +351,15 @@ class PythonGenerator(NodeVisitor):
         self.module_args = node._compiler_options
         mainbody = self.body(node.body)
         body = list(self.preambles)
-        if node.configurations:
-            body.append(self.generate_config(node))
+        body.append(self.generate_config(node))
         body.extend(mainbody)
         body.extend(self.postambles)
         return Module(body)
 
     def generate_config(self, node):
-        return Expr(pyCall(pyAttr("da", CONFIG_API_NAME),
-                           [],
-                           [(key, self.visit(value))
-                            for key, value in node.configurations]))
+        return Assign([pyName(CONFIG_OBJECT_NAME)],
+                      Dict([Str(key) for key, _ in node.configurations],
+                           [self.visit(val) for _, val in node.configurations]))
 
     def generate_event_def(self, node):
         evtype = pyAttr(pyAttr("da", "pat"), node.type.__name__)

commit c70bb510e788fcb58ce236d6303616cf16c91e45

Restore behavior of `config` in `main`.
d7429a64989fcf26baaaaa550846327ff70f55bb broke backward compatibility by
compiling `config` in `main` into module-level configuration. This patch
restores the old behavior where calling `config` inside `main` applies
configuration *globally*, across all loaded DistAlgo modules. Any
conflicting configurations will be resolved according to the following
order, in decreasing priority:

  - `--config`(or `-o`) command line option
  - `config()` API (only callable from `main`)
  - Process-level configuration
  - Module-level configuration

* da/api.py(config): resurrected API (only visible in `main` due to name
  conflict with the `config` keyword).

* da/common.py:
  -(GlobalConfig): new dict, to keep track of `config` called from
   `main`.
  -(set_global_config, global_config): new functions to set and read
   `GlobalConfig`.

* da/compiler/parser.py(Parser.visit_Expr): parse `KW_CONFIG` inside
  `main` as an API call.

* da/sim.py(DistProcess.get_config): consult `common.global_config`.

da/api.py
@@ -178,6 +178,9 @@ def entrypoint():
         log.error("Caught unexpected global exception: %r", e)
         traceback.print_tb(err_info[2])
 
+@api
+def config(**props):
+    common.set_global_config(props)
 
 @api
 def new(pcls, args=None, num=None, **props):

da/common.py
@@ -38,6 +38,7 @@ from inspect import signature, Parameter
 from functools import wraps
 
 GlobalOptions = None
+GlobalConfig = dict()
 CurrentProcess = {'pid' : multiprocessing.current_process().pid}
 IncOQBaseType = None           # incoq.runtime.Type, if using incoq
 INCOQ_MODULE_NAME = "incoq.mars.runtime"
@@ -70,6 +71,12 @@ def set_current_process():
 def set_global_options(params):
     global GlobalOptions
     GlobalOptions = params
+
+def set_global_config(props):
+    GlobalConfig.update(props)
+
+def global_config():
+    return GlobalConfig
 
 def global_options():
     return GlobalOptions

da/compiler/parser.py
@@ -1154,9 +1154,8 @@ class Parser(NodeVisitor):
                                    "Valid arguments are: " +
                                    str(ValidResetTypes), node)
 
-            # Parse 'config' statements. These may appear at the module level,
-            # process level, or in the 'main' function (for backwards
-            # compatability):
+            # Parse 'config' statements. These may appear at the module level or
+            # the process level:
             elif self.expr_check(KW_CONFIG, 0, None, e, keywords=None):
                 if isinstance(self.current_parent, dast.Process):
                     self.current_process.configurations.extend(
@@ -1164,11 +1163,14 @@ class Parser(NodeVisitor):
                 elif isinstance(self.current_parent, dast.Program):
                     self.current_parent.configurations.extend(
                         self.parse_config_section(e))
+                # If 'config' occurs in the 'main' function, treat it as an
+                # ApiCallExpr (for backwards compatability):
                 elif (isinstance(self.current_parent, dast.Function) and
                       self.current_parent.name == KW_ENTRY_POINT and
                       isinstance(self.current_parent.parent, dast.Program)):
-                    self.current_parent.parent.configurations.extend(
-                        self.parse_config_section(e))
+                    stmtobj = self.create_stmt(dast.SimpleStmt, node)
+                    self.current_context = Read(stmtobj)
+                    stmtobj.expr = self.visit(node.value)
                 else:
                     self.error("Invalid context for '%s' statement." %
                                KW_CONFIG, node)

da/sim.py
@@ -138,6 +138,8 @@ class DistProcess():
         """
         if key in common.global_options().config:
             return common.global_options().config[key]
+        elif key in common.global_config():
+            return common.global_config()[key]
         elif key in cls._config_object:
             return cls._config_object[key]
         elif key in sys.modules[cls.__module__]._config_object: