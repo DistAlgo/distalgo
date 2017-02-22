5e9a214e2698dcc5e3b099a82e8991b039072af5
Bo Lin <bo@dreamsphere.org>
2017-02-22 01:09:14 -0500

    Bump version to 1.0.8.

--- da/common.py
+++ da/common.py
@@ -37,7 +37,7 @@ from functools import wraps
 
 MAJOR_VERSION = 1
 MINOR_VERSION = 0
-PATCH_VERSION = 7
+PATCH_VERSION = 8
 PRERELEASE_VERSION = ""
 __version__ = "{}.{}.{}{}".format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION,
                                    PRERELEASE_VERSION)
