DistAlgo/distalgo

commit b3fb2ef1f8b0ec3b716669be7f46ca734eb6589a

Fix potential infinite loop in router.
When routing a message to a process id that is not in Router.local_procs but
has the same transport address as the local TransportManager (this will
happen when e.g. the destination process has already terminated), if the
router forwards the message to the TransportManager it will come right back
through Router.incomingq, causing an infinite loop. This patch prevents this
by discarding messages that will cause infinite circular routing.

* da/sim.py:
  -(CircularRoutingException): new exception class.
  -(Router):
   -(._send_remote): check whether destination is actually remote (i.e. has
    different transport address as local TransportManager)
   -(._dispatch): catch and log CircularRoutingException.

da/sim.py
@@ -982,6 +982,7 @@ class NodeProcess(DistProcess):
                         message=self._id, to=self._nodes)
 
 class RoutingException(Exception): pass
+class CircularRoutingException(RoutingException): pass
 class BootstrapException(RoutingException): pass
 class NoAvailableTransportException(RoutingException): pass
 class MessageTooBigException(RoutingException): pass
@@ -1140,6 +1141,13 @@ class Router(threading.Thread):
                        mesg, dest, flags)
         if dest.hostname != self.hostname:
             flags |= ChannelCaps.INTERHOST
+        elif dest.transports == self.endpoint.transport_addresses:
+            # dest is not in our local_procs but has same hostname and transport
+            # address, so most likely dest is a process that has already
+            # terminated. Do not attempt forwarding or else will cause infinite
+            # loop:
+            raise CircularRoutingException('destination: {}'.format(dest))
+
         if transport is None:
             transport = self.endpoint.get_transport(flags)
         if transport is None:
@@ -1188,6 +1196,11 @@ class Router(threading.Thread):
             try:
                 self._send_remote(src, dest, payload, flags, **params)
                 return True
+            except CircularRoutingException as e:
+                # This is most likely due to stale process ids, so don't log
+                # error, just debug:
+                self.log.debug("Caught %r.", e)
+                return False
             except Exception as e:
                 self.log.error("Could not send message due to: %r", e)
                 self.log.debug("Send failed: ", exc_info=1)

commit e685cb39c54e44bbc581d291a61f7ed2c035aad2

Fix CircularRoutingException during bootstrap.
This patch fixes a bug introduced by
b3fb2ef1f8b0ec3b716669be7f46ca734eb6589a, in which a
CircularRoutingException causes bootstrapping to fail if the user does not
explicitly specify '--master'. This exception is benign during bootstrap
since we're just blindly trying out potential master node addresses using a
dummy process id, so we should just treat it the same as TransportException,
i.e. log it and move on.

* da/sim.py(Router.bootstrap_node): catch `CircularRoutingException`.

da/sim.py
@@ -1085,7 +1085,7 @@ class Router(threading.Thread):
             except endpoint.AuthenticationException as e:
                 # Abort immediately:
                 raise e
-            except endpoint.TransportException as e:
+            except (CircularRoutingException, endpoint.TransportException) as e:
                 self.log.debug("Bootstrap attempt to %s:%d with %r failed "
                                ": %r", hostname, port, transport, e)
         if self.bootstrap_peer is None: