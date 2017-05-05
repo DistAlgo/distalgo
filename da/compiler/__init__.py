8b74f8a36491732715950820aea6ad8888ba4276
Bo Lin <bo@dreamsphere.org>
2017-05-04 23:38:53 -0400

    compiler ui: implement `dafile_to_pycfile`.
    As a continued effort to complete 440015a2cded692b7dd3124a0e8e5ae6150f0089,
    this patch adds a `dafile_to_pycfile` auxiliary function that generates a
    Python bytecode file from a DistAlgo source file. Also refactored some other
    parts of the compiler ui to make the interface more uniform.
    
    * da/compiler/__init__.py: import `ui.dafile_to_pycfile` and add it to
      `__all__`.
    
    * da/compiler/ui.py:
      -(dafile_to_pyast): bug fix: must bail when `pyast` is None; print a dump
       of generated Python AST if desired.
      -(_pyast_to_pycode): new auxiliary function.
      -(dafile_to_pycode): use `_pyast_to_pycode`; allow "purported
       filename"(dfile) to be different from actual source filename, which is
       useful for example in generating binary distributions.
      -(dastr_to_pycode): use `_pyast_to_pycode`.
      -(_sanitize_filename): new auxiliary function.
      -(dafile_to_pseudofile): use `_sanitize_filename`.
      -(dafile_to_pyfile): unify calling interface; use `_sanitize_filename`.
      -(dafile_to_pycfile): new function.
      -(main): new arguments '-O' and '-C'.

--- da/compiler/__init__.py
+++ da/compiler/__init__.py
@@ -8,10 +8,12 @@ from .ui import dafile_to_pyast
 from .ui import dafile_to_pyfile
 from .ui import dafile_to_pycode
 from .ui import dastr_to_pycode
+from .ui import dafile_to_pycfile
 from .ui import main
 
 __all__ = ['PythonGenerator', 'Parser',
            'daast_from_file', 'daast_from_str',
            'dafile_to_pyast', 'dafile_to_pyfile',
            'dafile_to_pycode', 'dastr_to_pycode',
+           'dafile_to_pycfile',
            'main']
