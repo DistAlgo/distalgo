DistAlgo/distalgo

commit 673dd93c17725393b3e658a6c68fb746defb7bd7

Make `initialize_runtime_options` more resilient.
* da/__main__.py: clean up imports.

* da/common.py:
  - clean up imports.
  -(GlobalOptions): now a `defaultdict`.
  -(initialize_runtime_options): allow `options` to be None; allow multiple
   calls.

da/common.py
@@ -33,9 +33,7 @@ import warnings
 import threading
 
 from datetime import datetime
-from collections import abc
-from collections import deque
-from collections import namedtuple
+from collections import abc, deque, namedtuple, defaultdict
 from inspect import signature
 from inspect import Parameter
 from functools import wraps
@@ -51,7 +49,7 @@ __version__ = "{}.{}.{}{}".format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION,
 INCOQ_MODULE_NAME = "incoq.mars.runtime"
 
 # a dict that contains the runtime configuration values:
-GlobalOptions = None
+GlobalOptions = defaultdict((lambda: None))
 # a dict containing configuration overrides set by the `config` API:
 GlobalConfig = dict()
 # Process id of the node process:
@@ -110,30 +108,29 @@ VERSION_BYTES = _version_as_bytes()
 
 def _parse_items(items):
     subs = dict()
-    for item in items:
-        parts = item.split(':')
-        if len(parts) != 2:
-            raise InvalidStateException('unrecognized substitute spec: {}'
-                                        .format(item))
-        subs[parts[0]] = parts[1]
+    if items:
+        for item in items:
+            parts = item.split(':')
+            if len(parts) != 2:
+                raise InvalidStateException('unrecognized substitute spec: {}'
+                                            .format(item))
+            subs[parts[0]] = parts[1]
     return subs
 
-def initialize_runtime_options(options):
+def initialize_runtime_options(options=None):
     """Sets and sanitizes runtime options.
 
     'options' should be a dict-like object containing mappings from options
     names to corresponding values.
 
-    This function has to be called exactly once per node instance.
-
     """
-    global GlobalOptions
-    if GlobalOptions is not None:
-        raise InvalidStateException("DistAlgo is already initialized!")
+    if options:
+        GlobalOptions.update(options)
 
-    GlobalOptions = dict(options)
     # Canonize hostname, essential for properly determining whether a ProcessId
     # is running on the local machine:
+    if GlobalOptions['nodename'] is None:
+        GlobalOptions['nodename'] = ''
     import socket
     if GlobalOptions['hostname'] is None:
         if len(GlobalOptions['nodename']) > 0:
@@ -157,9 +154,12 @@ def initialize_runtime_options(options):
 
     # Convert 'compiler_flags' to a namespace object that can be passed directly
     # to the compiler:
+    if GlobalOptions['compiler_args'] is None:
+        GlobalOptions['compiler_args'] = ""
     from . import compiler
     GlobalOptions['compiler_args'] \
-        = compiler.ui.parse_compiler_args(GlobalOptions['compiler_flags'].split())
+        = compiler.ui.parse_compiler_args(
+            GlobalOptions['compiler_flags'].split())
 
     # Make sure the directory for storing trace files exists:
     if GlobalOptions['record_trace']:

da/__main__.py
@@ -28,7 +28,7 @@ import argparse
 
 from datetime import datetime
 
-from .api import entrypoint, DEFAULT_MASTER_PORT, dump_trace
+from .api import entrypoint
 from . import common
 
 MINIMUM_PYTHON_VERSION = (3, 5)

commit 1bef8a04bcc29e816d4fac9218dfd58eb738d736

Fix process spawning bug.
Fix bug introduced by 673dd93c17725393b3e658a6c68fb746defb7bd7 which broke
--start-method=spawn.

* da/common.py:
  -(GlobalOptions): revert to None. Don't initialize to `defaultdict`
   because it's not pickleable. Instead, change all dependent code to not
   assume the presence of keys in `GlobalOptions`.
  -(initialize_runtime_options): don't assume presence of keys in
  `GlobalOptions`.

da/common.py
@@ -33,7 +33,7 @@ import warnings
 import threading
 
 from datetime import datetime
-from collections import abc, deque, namedtuple, defaultdict
+from collections import abc, deque, namedtuple
 from inspect import signature
 from inspect import Parameter
 from functools import wraps
@@ -49,7 +49,7 @@ __version__ = "{}.{}.{}{}".format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION,
 INCOQ_MODULE_NAME = "incoq.mars.runtime"
 
 # a dict that contains the runtime configuration values:
-GlobalOptions = defaultdict((lambda: None))
+GlobalOptions = None
 # a dict containing configuration overrides set by the `config` API:
 GlobalConfig = dict()
 # Process id of the node process:
@@ -124,15 +124,18 @@ def initialize_runtime_options(options=None):
     names to corresponding values.
 
     """
+    global GlobalOptions
+    if not GlobalOptions:
+        GlobalOptions = dict()
     if options:
         GlobalOptions.update(options)
 
     # Canonize hostname, essential for properly determining whether a ProcessId
     # is running on the local machine:
-    if GlobalOptions['nodename'] is None:
+    if GlobalOptions.get('nodename') is None:
         GlobalOptions['nodename'] = ''
     import socket
-    if GlobalOptions['hostname'] is None:
+    if GlobalOptions.get('hostname') is None:
         if len(GlobalOptions['nodename']) > 0:
             GlobalOptions['hostname'] = socket.getfqdn()
         else:
@@ -142,27 +145,27 @@ def initialize_runtime_options(options=None):
 
     # Parse '--substitute-classes' and '--substitute-modules':
     GlobalOptions['substitute_classes'] = \
-                            _parse_items(GlobalOptions['substitute_classes'])
+                            _parse_items(GlobalOptions.get('substitute_classes'))
     GlobalOptions['substitute_modules'] = \
-                            _parse_items(GlobalOptions['substitute_modules'])
+                            _parse_items(GlobalOptions.get('substitute_modules'))
 
     # Configure multiprocessing package to use chosen semantics:
     import multiprocessing
-    startmeth = GlobalOptions['start_method']
+    startmeth = GlobalOptions.get('start_method')
     if startmeth != multiprocessing.get_start_method(allow_none=True):
         multiprocessing.set_start_method(startmeth)
 
     # Convert 'compiler_flags' to a namespace object that can be passed directly
     # to the compiler:
-    if GlobalOptions['compiler_args'] is None:
-        GlobalOptions['compiler_args'] = ""
     from . import compiler
     GlobalOptions['compiler_args'] \
         = compiler.ui.parse_compiler_args(
-            GlobalOptions['compiler_flags'].split())
+            GlobalOptions.get('compiler_flags', '').split())
 
     # Make sure the directory for storing trace files exists:
-    if GlobalOptions['record_trace']:
+    if GlobalOptions.get('record_trace'):
+        if 'logdir' not in GlobalOptions:
+            raise ValueError("'record_trace' enabled without setting 'logdir'")
         os.makedirs(GlobalOptions['logdir'], exist_ok=True)
 
 def set_global_config(props):